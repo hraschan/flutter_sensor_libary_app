6e208483dfe91a45c1fac52143914a2533b32b4e
armin-nova <47531709+armin-nova@users.noreply.github.com>
2021-11-27 17:26:58 +0100

    added compass to new asset folder and implemented rotation

--- lib/routes/kompass_route.dart
+++ lib/routes/kompass_route.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
-class KompassRoute extends StatelessWidget {
+class KompassRoute extends StatefulWidget {
   const KompassRoute({Key? key}) : super(key: key);
+
+ 
+  @override
+  _KompassRouteState createState() => _KompassRouteState();
+}
+
+class _KompassRouteState extends State<KompassRoute> {
 
   @override
   Widget build(BuildContext context) {
+    const pi = 3.14159265359;
+    var direction = 45; 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Kompass"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: Transform.rotate(angle: ((direction) * (pi / 180) * -1),
+        child: Image.asset('assets/images/compass.jpg'),
       ),
-    );
+    ));
   }
 }
