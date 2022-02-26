f0f0907c614bb5c149d20a91ac42fbd40be48e73
BettinaMaria98 <bettina.broethaler@gmx.at>
2022-02-26 16:17:17 +0100

    Added proximity Route

--- lib/routes/proximitiy_route.dart
+++ lib/routes/proximitiy_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sensor_libary_test_app/routes/proximity_recording_route.dart';
+import 'package:sensor_libary_test_app/themes/custom_colors.dart';
 import 'package:sensor_libary_test_app/widgets/record_button_widget.dart';
 
 class ProximitiyRoute extends StatelessWidget {
@@ -7,6 +8,9 @@ class ProximitiyRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var currentDistance = 0;
+    var limit = 5;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Proximitiy'),
@@ -14,8 +18,40 @@ class ProximitiyRoute extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(25.0),
         child: Column(
-          children: const [
-            RecordButtonWidget(route: ProximityRecordingRoute())
+          children: [
+            const RecordButtonWidget(route: ProximityRecordingRoute()),
+            Column(
+              children: [
+                const SizedBox(height: 25),
+                Text("Grenze: $limit CM"),
+                const SizedBox(height: 20),
+                Stack(children: const [
+                  Positioned(
+                      child: Icon(Icons.smartphone,
+                          size: 300, color: SecondaryColor)),
+                  Positioned(
+                      right: 1,
+                      left: 1,
+                      top: 1,
+                      bottom: 1,
+                      child: Icon(Icons.lightbulb,
+                          size: 150, color: PrimaryColor)),
+                ]),
+                const SizedBox(height: 20),
+                const Text(
+                  "Aktueller Abstand",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  "$currentDistance CM",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: PrimaryColor,
+                      fontSize: 20),
+                )
+              ],
+            )
           ],
         ),
       ),
