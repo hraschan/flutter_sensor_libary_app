eaf837004c068d7f2613d1729ecbb773606b81c5
BettinaMaria98 <bettina.broethaler@gmx.at>
2022-02-27 17:35:11 +0100

    Updated Barometer with Library Data

--- lib/routes/barometer_route.dart
+++ lib/routes/barometer_route.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:sensor_libary_test_app/widgets/coming_soon_info_widget.dart';
+import 'package:sensor_libary_test_app/themes/custom_colors.dart';
+import 'package:sensor_library/models/raw_sensors/barometer.dart';
 
-class BarometerRoute extends StatelessWidget {
+class BarometerRoute extends StatefulWidget {
   const BarometerRoute({Key? key}) : super(key: key);
+
+  @override
+  _BarometerRouteState createState() => _BarometerRouteState();
+}
+
+class _BarometerRouteState extends State<BarometerRoute> {
+  double hpa = 0.0;
+  double mmm = 0.0;
+  late Barometer barometer;
+
+  @override
+  void initState() {
+    super.initState();
+    barometer = Barometer(inMillis: 500);
+    barometer.getRaw().listen((element) {
+      double valueInHectopascal = element.hectpascal;
+      double valueInMMMercury = element.millimeterOfMercury;
+
+      setState(() {
+        hpa = valueInHectopascal;
+        mmm = valueInMMMercury;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +35,43 @@ class BarometerRoute extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Barometer"),
         ),
-        body: const ComingSoonInfoWidget());
+        body: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 5),
+                Center(
+                    child: Text("hectpascal".toUpperCase(),
+                        style: const TextStyle(fontWeight: FontWeight.bold))),
+                Center(
+                    child: Text(
+                  hpa.toStringAsFixed(2),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: PrimaryColor,
+                      fontSize: 30),
+                )),
+                const SizedBox(height: 30),
+                Center(
+                    child: Text("Millimeter Of Mercury".toUpperCase(),
+                        style: const TextStyle(fontWeight: FontWeight.bold))),
+                Center(
+                    child: Text(
+                  mmm.toStringAsFixed(2),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: PrimaryColor,
+                      fontSize: 30),
+                )),
+                const SizedBox(height: 30),
+                const Center(
+                    child: Text(
+                  "All Values fixed to 2 Decimals",
+                  style: TextStyle(fontSize: 14),
+                )),
+              ],
+            )));
   }
 }
