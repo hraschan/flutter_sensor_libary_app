62a5cf8dcf82cd3b742b74f297b890dc619724fd
BettinaMaria98 <bettina.broethaler@gmx.at>
2022-03-03 11:55:17 +0100

    Added Icon to visulize Data within Lightsensor

--- lib/routes/lightsensor_route.dart
+++ lib/routes/lightsensor_route.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensor_libary_test_app/themes/custom_colors.dart';
 // import 'package:sensor_libary_test_app/widgets/coming_soon_info_widget.dart';
 import 'package:sensor_library/models/raw_sensors/light_sensor.dart';
 
@@ -12,6 +13,8 @@ class LightsensorRoute extends StatefulWidget {
 class _LightsensorRouteState extends State<LightsensorRoute> {
   int lightValue = 0;
   double liDoub = 0;
+  var currentIcon = Icons.lightbulb_outline;
+  var iconColor = SecondaryColor;
 
   @override
   void initState() {
@@ -19,25 +22,90 @@ class _LightsensorRouteState extends State<LightsensorRoute> {
 
     LightSensor lightSensor = LightSensor(inMillis: 500);
     lightSensor.getRawWithoutTimeLimit().listen((event) {
+      var lValue = event;
+      var icon = Icons.lightbulb_outline;
+      var color = SecondaryColor;
+
+      if (lValue != 0) {
+        if (lValue <= 50) {
+          icon = Icons.lightbulb_outline;
+          color = SecondaryColor;
+        }
+        if (lValue <= 1000 && lValue > 50) {
+          icon = Icons.lightbulb;
+          color = SecondaryColor;
+        }
+        if (lValue > 1000) {
+          icon = Icons.lightbulb;
+          color = PrimaryColor;
+        }
+      }
+
       if (mounted) {
-        setState(() => {lightValue = event, liDoub = lightValue.toDouble()});
-        // doubleState(() => {liDoub = event as double);
-        // print(lightValue);
+        setState(() {
+          lightValue = lValue;
+          liDoub = lValue.toDouble();
+          currentIcon = icon;
+          iconColor = color;
+        });
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    const space = 20.0;
+    var tablePadding = const EdgeInsets.all(15.0);
+
+    const tableBorder = TableBorder(
+        horizontalInside:
+            BorderSide(width: 2, color: Background, style: BorderStyle.solid),
+        verticalInside:
+            BorderSide(width: 2, color: Background, style: BorderStyle.solid));
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Lightsensor'),
         ),
-        body: Text(
-          'Sensor Value: $lightValue',
-          textAlign: TextAlign.center,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(height: 5, fontSize: 20),
-        ));
+        body: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                  const SizedBox(height: space * 3),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Icon(currentIcon, size: 150, color: iconColor),
+                  ),
+                  const SizedBox(height: space * 3),
+                  Table(
+                    border: tableBorder,
+                    children: [
+                      TableRow(
+                          decoration: const BoxDecoration(color: AccentWhite),
+                          children: [
+                            TableCell(
+                              child: Padding(
+                                  padding: tablePadding,
+                                  child: const Text('Lightvalue',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))),
+                            ),
+                            TableCell(
+                                child: Padding(
+                              padding: tablePadding,
+                              child: Text(
+                                "$lightValue",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: PrimaryColor),
+                              ),
+                            ))
+                          ]),
+                    ],
+                  ),
+                ]))));
   }
 }
